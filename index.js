b9eb5590dd77e1fbca144ce961f24bb3b4935af8
Louanne Monnier <lmonnier005@ensc.fr>
2025-04-14 14:54:14 +0200

    ciao commentaires

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { registerRootComponent } from 'expo';
 import App from './src/App'; // Chemin relatif correct
 
-// Enveloppez App avec des Providers si nécessaire (Navigation, Redux, etc.)
-console.log('✅ Index.js loaded'); // Debug
 
 registerRootComponent(App);
